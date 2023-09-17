baafad210266a9225e8ccca3f8330d30931cd571
carolsbraz <carolinesoares47@gmail.com>
2023-09-17 16:38:53 -0300

    Atividade 01 e 02 de NodeJs

---
+++ WACAD-007/nodeES/index.js
@@ -0,0 +1,36 @@
+import http from 'http';
+import fs from 'fs';
+import { createLink } from './utils.js';
+
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+if (process.argv.length < 3) {
+    throw new Error("Número de parâmetros inválidos");
+}
+
+const PORT = process.env.PORT ?? 6767;
+
+const dir = process.argv[2];
+
+const server = http.createServer((req, res) => {
+    res.writeHead(200, { "Content-type": "text/html;charset=utf-8" });
+    if (req.url === "/") {
+        fs.readdir(dir, (err, files) => {
+            if (err) throw new Error(err);
+            files.forEach(file => { res.write(`${createLink(dir, file)}`); });
+            res.end();
+        })
+    } else {
+        res.write(`<a href="/">Voltar</a><br>`);
+        fs.readFile(`.${req.url}`, "utf-8", (err, content) => {
+            if (err) throw new Error(err);
+            res.end(content);
+        });
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Servidor iniciado na porta ${PORT}`);
+});
