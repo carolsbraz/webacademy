9c3c5d3116639fc86b05af2c274ebf3f57c72f80
carolsbraz <carolinesoares47@gmail.com>
2023-09-20 10:49:48 -0300

    Lista 01 de DOM

---
+++ WACAD-004/lista-2/01/lista2.js
@@ -0,0 +1,11 @@
+const lista = document.getElementById('lista-itens-1');
+
+console.log("===== QUESTÃO A =====");
+console.log(lista.firstElementChild);
+console.log(lista.firstElementChild);
+
+console.log("===== QUESTÃO B =====");
+console.log(lista.parentElement);
+
+console.log("===== QUESTÃO C =====");
+
