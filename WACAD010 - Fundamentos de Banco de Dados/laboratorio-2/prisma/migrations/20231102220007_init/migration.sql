0d0bc38c8e884f50b85728e2f4ecbda5706c80bf
carolsbraz <carolinesoares47@gmail.com>
2023-11-02 19:01:15 -0300

    Laboratório 2 de Fundamentos de Banco de Dados

---
+++ WACAD010 - Fundamentos de Banco de Dados/laboratorio-2/prisma/migrations/20231102220007_init/migration.sql
@@ -0,0 +1,101 @@
+-- CreateTable
+CREATE TABLE `Cliente` (
+    `cpf` VARCHAR(191) NOT NULL,
+    `primeiroNome` VARCHAR(191) NOT NULL,
+    `meioNome` VARCHAR(191) NOT NULL,
+    `ultimoNome` VARCHAR(191) NOT NULL,
+    `nascimento` DATETIME(3) NOT NULL,
+    `telefone` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`cpf`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Endereco` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `rua` VARCHAR(191) NOT NULL,
+    `bairro` VARCHAR(191) NOT NULL,
+    `numero` INTEGER NOT NULL,
+    `cidade` VARCHAR(191) NOT NULL,
+    `estado` VARCHAR(191) NOT NULL,
+    `cpfCliente` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Categoria` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Subcategoria` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nome` VARCHAR(191) NOT NULL,
+    `idCategoria` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Produto` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `quantidade` INTEGER NOT NULL DEFAULT 0,
+    `modelo` VARCHAR(191) NOT NULL,
+    `fabricante` VARCHAR(191) NOT NULL,
+    `preco` DECIMAL(9, 2) NOT NULL,
+    `idSubcategoria` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Serie` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `numero` VARCHAR(191) NOT NULL,
+    `idProduto` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Compra` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `data` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `desconto` DOUBLE NOT NULL,
+    `formaPagamento` ENUM('Debito', 'Credito', 'Pix', 'Boleto') NOT NULL,
+    `totalCompra` DECIMAL(9, 2) NOT NULL,
+    `listaId` INTEGER NOT NULL,
+
+    UNIQUE INDEX `Compra_listaId_key`(`listaId`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Lista` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `idProduto` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Endereco` ADD CONSTRAINT `Endereco_cpfCliente_fkey` FOREIGN KEY (`cpfCliente`) REFERENCES `Cliente`(`cpf`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Subcategoria` ADD CONSTRAINT `Subcategoria_idCategoria_fkey` FOREIGN KEY (`idCategoria`) REFERENCES `Categoria`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Produto` ADD CONSTRAINT `Produto_idSubcategoria_fkey` FOREIGN KEY (`idSubcategoria`) REFERENCES `Subcategoria`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Serie` ADD CONSTRAINT `Serie_idProduto_fkey` FOREIGN KEY (`idProduto`) REFERENCES `Produto`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Compra` ADD CONSTRAINT `Compra_listaId_fkey` FOREIGN KEY (`listaId`) REFERENCES `Lista`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Lista` ADD CONSTRAINT `Lista_idProduto_fkey` FOREIGN KEY (`idProduto`) REFERENCES `Produto`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
